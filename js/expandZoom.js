2f9fe94b0506041d23b40d4705874c8d24b08829
ZhangBowei <919270881@qq.com>
2020-08-01 22:04:18 +0800

    Change

--- js/expandZoom.js
+++ js/expandZoom.js
@@ -1,3 +1,7 @@
-window.onload = function () {
-  document.body.querySelector('#page-container').style.zoom = '140%';
-}
+var start = new Date;
+setTimeout(function() {
+    var end = new Date;
+    console.log('Time elapsed:', end - start, 'ms');
+}, 500);
+while (new Date - start < 1000) {};
+
